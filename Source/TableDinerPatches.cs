410ceb6942263762053aa024c5bb73be427d8981
Antaios <antaiozi@gmail.com>
2021-08-04 23:11:26 +0800

    1.3 update
    - carryIngestibleToChewSpot rewritten to copy base game changes

--- Source/TableDinerPatches.cs
+++ Source/TableDinerPatches.cs
@@ -186,115 +186,127 @@ namespace Table_Diner_Configurable
 			{
 				return true;
 			}
-			Toil toil = new Verse.AI.Toil();
-			toil.initAction = delegate
-			{
-				Pawn actor = toil.actor;
-				IntVec3 intVec = IntVec3.Invalid;
-				Thing thing = null;
-				Thing thing2 = actor.CurJob.GetTarget(ingestibleInd).Thing;
-				Predicate<Thing> baseChairValidator = delegate (Thing t)
-				{
-					bool result;
-					if (t.def.building == null || !t.def.building.isSittable)
-					{
-						result = false;
-					}
-					else if (t.IsForbidden(pawn))
-					{
-						result = false;
-					}
-					else if (!actor.CanReserve(t, 1, -1, null, false))
-					{
-						result = false;
-					}
-					else if (!t.IsSociallyProper(actor))
-					{
-						result = false;
-					}
-					else if (t.IsBurning())
-					{
-						result = false;
-					}
-					else if (t.HostileTo(pawn))
-					{
-						result = false;
-					}
-					else
-					{
-						bool flag = false;
-						for (int i = 0; i < 4; i++)
-						{
-							IntVec3 c = t.Position + GenAdj.CardinalDirections[i];
-							Building edifice = c.GetEdifice(t.Map);
-							if (edifice != null && edifice.def.surfaceType == SurfaceType.Eat)
-							{
-								float tr = TableDinerGlobal.GetTableRadius(edifice.ThingID);
-								float pr = TableDinerGlobal.GetTableRadius(actor.ThingID);
+            Toil toil = new Toil();
+            toil.initAction = delegate
+            {
+                Pawn actor = toil.actor;
+                IntVec3 cell2 = IntVec3.Invalid;
+                Thing thing = null;
+                Thing thing2 = actor.CurJob.GetTarget(ingestibleInd).Thing;
+                Predicate<Thing> baseChairValidator = delegate (Thing t)
+                {
+                    if (t.def.building == null || !t.def.building.isSittable)
+                    {
+                        return false;
+                    }
+                    if (!TryFindFreeSittingSpotOnThing(t, out var _))
+                    {
+                        return false;
+                    }
+                    if (t.IsForbidden(pawn))
+                    {
+                        return false;
+                    }
+                    if (!actor.CanReserve(t))
+                    {
+                        return false;
+                    }
+                    if (!t.IsSociallyProper(actor))
+                    {
+                        return false;
+                    }
+                    if (t.IsBurning())
+                    {
+                        return false;
+                    }
+                    if (t.HostileTo(pawn))
+                    {
+                        return false;
+                    }
+                    bool flag = false;
+                    for (int i = 0; i < 4; i++)
+                    {
+                        IntVec3 c = t.Position + GenAdj.CardinalDirections[i];
+                        Building edifice = c.GetEdifice(t.Map);
+                        if (edifice != null && edifice.def.surfaceType == SurfaceType.Eat)
+                        {
+                            float tr = TableDinerGlobal.GetTableRadius(edifice.ThingID);
+                            float pr = TableDinerGlobal.GetTableRadius(actor.ThingID);
 
-								if (tr >= 1 || pr >= 1)
-								{
-									float r2 = (edifice.TrueCenter() - actor.TrueCenter()).sqrMagnitude;
-									if (tr < 1)
-									{
-										if (r2 <= Mathf.Pow(pr, 2))
-										{
-											flag = true;
-											break;
-										}
-									}
-									else if (pr < 1)
-									{
-										if (r2 <= Mathf.Pow(tr, 2))
-										{
-											flag = true;
-											break;
-										}
-									}
-									else
-									{
-										if (r2 <= Mathf.Pow(Mathf.Min(tr, pr), 2))
-										{
-											flag = true;
-											break;
-										}
-									}
-								}
-								else
-								{
-									flag = true;
-									break;
-								}
-							}
-						}
-						result = flag;
-					}
-					return result;
-				};
-				if (thing2.def.ingestible.chairSearchRadius > 0f)
-				{
-					thing = GenClosest.ClosestThingReachable(actor.Position, actor.Map, ThingRequest.ForGroup(ThingRequestGroup.BuildingArtificial), PathEndMode.OnCell, TraverseParms.For(actor, Danger.Deadly, TraverseMode.ByPawn, false), thing2.def.ingestible.chairSearchRadius, (Thing t) => baseChairValidator(t) && t.Position.GetDangerFor(pawn, t.Map) == Danger.None, null, 0, -1, false, RegionType.Set_Passable, false);
-				}
-				if (thing == null)
-				{
-					intVec = RCellFinder.SpotToChewStandingNear(actor, actor.CurJob.GetTarget(ingestibleInd).Thing);
-					Danger chewSpotDanger = intVec.GetDangerFor(pawn, actor.Map);
-					if (chewSpotDanger != Danger.None)
-					{
-						thing = GenClosest.ClosestThingReachable(actor.Position, actor.Map, ThingRequest.ForGroup(ThingRequestGroup.BuildingArtificial), PathEndMode.OnCell, TraverseParms.For(actor, Danger.Deadly, TraverseMode.ByPawn, false), thing2.def.ingestible.chairSearchRadius, (Thing t) => baseChairValidator(t) && t.Position.GetDangerFor(pawn, t.Map) <= chewSpotDanger, null, 0, -1, false, RegionType.Set_Passable, false);
-					}
-				}
-				if (thing != null)
-				{
-					intVec = thing.Position;
-					actor.Reserve(thing, actor.CurJob, 1, -1, null);
-				}
-				actor.Map.pawnDestinationReservationManager.Reserve(actor, actor.CurJob, intVec);
-				actor.pather.StartPath(intVec, PathEndMode.OnCell);
-			};
-			toil.defaultCompleteMode = ToilCompleteMode.PatherArrival;
-			__result = toil;
-			return false;
+                            if (tr >= 1 || pr >= 1)
+                            {
+                                float r2 = (edifice.TrueCenter() - actor.TrueCenter()).sqrMagnitude;
+                                if (tr < 1)
+                                {
+                                    if (r2 <= Mathf.Pow(pr, 2))
+                                    {
+                                        flag = true;
+                                        break;
+                                    }
+                                }
+                                else if (pr < 1)
+                                {
+                                    if (r2 <= Mathf.Pow(tr, 2))
+                                    {
+                                        flag = true;
+                                        break;
+                                    }
+                                }
+                                else
+                                {
+                                    if (r2 <= Mathf.Pow(Mathf.Min(tr, pr), 2))
+                                    {
+                                        flag = true;
+                                        break;
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                flag = true;
+                                break;
+                            }
+                        }
+                    }
+                    return flag ? true : false;
+                };
+                if (thing2.def.ingestible.chairSearchRadius > 0f)
+                {
+                    thing = GenClosest.ClosestThingReachable(actor.Position, actor.Map, ThingRequest.ForGroup(ThingRequestGroup.BuildingArtificial), PathEndMode.OnCell, TraverseParms.For(actor), thing2.def.ingestible.chairSearchRadius, (Thing t) => baseChairValidator(t) && t.Position.GetDangerFor(pawn, t.Map) == Danger.None);
+                }
+                if (thing == null)
+                {
+                    cell2 = RCellFinder.SpotToChewStandingNear(actor, actor.CurJob.GetTarget(ingestibleInd).Thing, (IntVec3 c) => actor.CanReserveSittableOrSpot(c));
+                    Danger chewSpotDanger = cell2.GetDangerFor(pawn, actor.Map);
+                    if (chewSpotDanger != Danger.None)
+                    {
+                        thing = GenClosest.ClosestThingReachable(actor.Position, actor.Map, ThingRequest.ForGroup(ThingRequestGroup.BuildingArtificial), PathEndMode.OnCell, TraverseParms.For(actor), thing2.def.ingestible.chairSearchRadius, (Thing t) => baseChairValidator(t) && (int)t.Position.GetDangerFor(pawn, t.Map) <= (int)chewSpotDanger);
+                    }
+                }
+                if (thing != null && !TryFindFreeSittingSpotOnThing(thing, out cell2))
+                {
+                    Log.Error("Could not find sitting spot on chewing chair! This is not supposed to happen - we looked for a free spot in a previous check!");
+                }
+                actor.ReserveSittableOrSpot(cell2, actor.CurJob);
+                actor.Map.pawnDestinationReservationManager.Reserve(actor, actor.CurJob, cell2);
+                actor.pather.StartPath(cell2, PathEndMode.OnCell);
+                bool TryFindFreeSittingSpotOnThing(Thing t, out IntVec3 cell)
+                {
+                    foreach (IntVec3 item in t.OccupiedRect())
+                    {
+                        if (actor.CanReserveSittableOrSpot(item))
+                        {
+                            cell = item;
+                            return true;
+                        }
+                    }
+                    cell = default(IntVec3);
+                    return false;
+                }
+            };
+            toil.defaultCompleteMode = ToilCompleteMode.PatherArrival;
+            __result = toil;
+            return false;
 		}
 	}
 }
